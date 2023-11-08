966b1ac1f44daa9b13d71584309d21fce3fc26ea
ishitagit2003 <ishitagupta2003@gmail.com>
2023-11-08 10:04:13 +0530

    jwt authentication and cookies added

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const path = require("path");
 const express= require("express");
+const mongoose = require("mongoose");
+
 
 const userRoute = require('./routes/user')
 
 const app=express();
 const port=8003;
 
+mongoose.connect('mongodb+srv://ishitagupta2003:qwerty12@cluster0.8z4ifni.mongodb.net/?retryWrites=true&w=majority')
+.then((e) => console.log("MongoDB Connected"));
+
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
+
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", async (req, res) => {
     // const allBlogs = await Blog.find({});
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const User = require("../models/user");
 
 const router=Router();
 
@@ -8,6 +9,19 @@ router.get("/signin", (req, res) => {
   
   router.get("/signup", (req, res) => {
     return res.render("signup");
+  });
+
+  router.post("/signin", async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      const token = await User.matchPasswordAndGenerateToken(email, password);
+  
+      return res.cookie("token", token).redirect("/");
+    } catch (error) {
+      return res.render("signin", {
+        error: "Incorrect Email or Password",
+      });
+    }
   });
 
   router.post("/signup", async (req, res) => {
