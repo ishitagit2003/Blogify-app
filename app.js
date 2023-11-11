2e7e8ab67030030630efb0449278e3651b812d3b
ishitagit2003 <ishitagupta2003@gmail.com>
2023-11-11 14:01:25 +0530

    middlewares added

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const path = require("path");
 const express= require("express");
 const mongoose = require("mongoose");
+const cookieParser = require('cookie-parser');
 
-
-const userRoute = require('./routes/user')
+const userRoute = require('./routes/user');
+const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 
 const app=express();
 const port=8003;
@@ -15,10 +16,15 @@ app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
 
 app.get("/", async (req, res) => {
     // const allBlogs = await Blog.find({});
-    res.render("home");
+    res.render("home", {
+      user: req.user,
+      // blogs: allBlogs,
+    });
   });
   
   app.use("/user", userRoute);
